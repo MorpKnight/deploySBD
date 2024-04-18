1a1c99bd73319d71089589893d53b9a056ab74d9
MorpKnight <christoffelsihombing@gmail.com>
2024-04-19 00:09:24 +0700

    add winston morgan helmet

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const eventRepo = require('./repositories/repository.event');
+const morganMiddleware = require('./src/middlewares/morgan.middleware');
+const helmet = require('helmet');
 
 const port = 6084;
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morganMiddleware);
+app.use(helmet());
+
+app.use(helmet.contentSecurityPolicy({
+    directives: {
+        defaultSrc: ["'self'"],
+        styleSrc: ["'self'", "'https://fonts.googleapis.com'"]
+    }
+}));
+
+app.use(helmet.frameguard({ action: 'deny' }));
+app.use(helmet.xssFilter());
+app.use(helmet.noSniff());
+app.use(helmet.hsts({ maxAge: 7776000000, preload: true }));
 
 app.post('/events', eventRepo.addEvent);
 app.get('/events', eventRepo.getAllEvents);
